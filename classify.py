7d4e93cfa8634c9a06840c3ef96026280f00046c
Devon Fritz <devon.s.fritz@gmail.com>
2015-10-31 10:07:20 +0100

    Initial commit

---
+++ classify.py
@@ -0,0 +1,123 @@
+# Author: Devon Fritz
+# This file classifies the provided text based on its word features using
+# SVM
+import argparse
+import csv
+from sklearn import svm
+import time
+from scipy.sparse import csr_matrix
+import numpy
+
+def timeit(f, no_params=False):
+
+    def timed(*args, **kw):
+
+        ts = time.time()
+        result = f(*args, **kw)
+        te = time.time()
+
+        print ("func:%r took: %2.4f sec" %
+               (f.__name__, te-ts))
+        return result
+
+    return timed
+
+
+class SVM_record:
+    """
+    This class represents one SVM record and stores internally its SVM
+    representation.
+    """
+    def __init__(self, array, all_words):
+        self.record_number = array[0]
+        self.polarity = 1 if int(array[1]) == 1 else -1
+        self.bag_of_words = self.calculate_bag_of_words(array[3], all_words)
+        self.orig_sentence = array[3]
+
+    def calculate_bag_of_words(self, message, all_words):
+        message_array = message.split()
+        bow = []
+
+        for word in all_words:
+            if word in message_array:
+                bow.append(1)
+            else:
+                bow.append(0)
+
+        return numpy.array(bow)
+
+
+def create_word_array(infile):
+    """Creates a huge word array that represents all words in the text"""
+    all_words = []
+
+    with open(infile) as csvfile:
+        reader = csv.reader(csvfile)
+        for line in reader:
+            all_words += line[3].split()
+
+    return all_words
+
+
+def process_file(infile, all_words, perc_test=20):
+    """Collects all rows and builds up the bag of words for them so that they
+    can feed the classifier"""
+    with open(infile) as csvfile:
+        reader = csv.reader(csvfile)
+        records = []
+        for line in reader:
+            records.append(SVM_record(line, all_words))
+
+    cutoff_record = int(len(records) * (perc_test/100))
+    test_records = numpy.array(records[0:cutoff_record])
+    training_records = numpy.array(records[cutoff_record:])
+    return training_records, test_records
+
+@timeit
+def train_classifier(records):
+    """This method takes the finished vector records we have build up and
+    feeds them to an SVM classifier based on their polarity
+    """
+    vectors = []
+    polarities = []
+
+    for r in records:
+        vectors.append(r.bag_of_words)
+        polarities.append(r.polarity)
+
+    print("Start training classifier")
+    clf = svm.SVC(kernel='linear')
+    clf.fit(vectors, polarities)
+
+    return clf
+
+
+@timeit
+def classify_unseen_records(classifier, records):
+    right = 0
+    total = 0
+    total_1 = 0
+    for r in records:
+        result = classifier.predict(r.bag_of_words)
+        if result[0] == r.polarity:
+            right += 1
+
+        if result[0] == 1:
+            total_1 += 1
+        total += 1
+
+    print("total 1s: %i" % total_1)
+    # print("expected: " + str(r.polarity) + " got: " + str(result[0]))
+
+    print("total: %f (%i/%i)" % ((right/float(total)), right, total))
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument("infile")
+    args = parser.parse_args()
+    infile = args.infile
+
+    all_words = list(set(create_word_array(infile)))
+    training_records, test_records = process_file(infile, all_words)
+    classifier = train_classifier(training_records)
+    results = classify_unseen_records(classifier, test_records)
---
+++ preprocess_sentences.py
@@ -0,0 +1,73 @@
+# Author: Devon Fritz
+# This script takes in lists of sentences and preprocesses them so that the
+# classifier can do a better job
+import argparse
+import csv
+import re
+
+
+def process_file(filename, handlers):
+    """
+    Takes in the file we want to process and does some preprocessing so that the
+    classifier can perform better.
+    """
+    with open(filename) as csvfile:
+        reader = csv.reader(csvfile)
+
+        for line in reader:
+            result = line[3]
+            for h in handlers:
+                result = h(result)
+
+            print(','.join(line[0:3]) + ', ' + result)
+
+
+def proc_multi_symbol(record):
+    """
+    Normalizes multiple symbols such as '!!!!!' to '!', since they express about
+    the same thing and it will cut down on our word count.
+    """
+    # if they have as least two dots, capture it as an expression of an
+    # ellipsis thought
+    CONST_ELLIPSIS = ' ELL_NORM'
+    record = re.sub(r'(\.\.+)', CONST_ELLIPSIS, record)
+
+    # normalize the exclamation marks in a similar way
+    CONST_EXCLAMATION = ' EXC_NORM'
+    record = re.sub(r'(!+)', CONST_EXCLAMATION, record)
+    return record
+
+
+def proc_handle_links(record):
+    """
+    Removes the links that are commonly found in tweets and replaces them
+    with a normalizing constant, so that the classifier can train more
+    effectively
+    """
+    CONST_LINK = 'LINK_NORM'
+
+    record = re.sub(r'http://[^\s]*', CONST_LINK, record)
+    return record
+
+
+def proc_handle_ats(record):
+    """
+    Removes the @someone links from Twitter's grammar and replaces them with a
+    normalizing constant, so that the classifier can train more effectively
+    """
+    CONST_AT = 'LINK_AT'
+
+    record = re.sub(r'@[^\s]*', CONST_AT, record)
+    return record
+
+# Main entry into the file - reads in our default file and preprocesses it, then
+# spits out the output which can be piped into a postprocessed file
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument("infile")
+    args = parser.parse_args()
+    infile = args.infile
+
+    process_file(infile, [proc_handle_ats,
+                          proc_handle_links,
+                          proc_multi_symbol])
